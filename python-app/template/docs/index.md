2d0008ecd55cc43158053beb9ebaf01918207d1c
Rod Stewart <rodstewart@gmail.com>
2025-05-26 09:13:34 -0500

    Adds python-app template

--- python-app/template/docs/index.md
+++ python-app/template/docs/index.md
@@ -1,4 +1,4 @@
-# Documentation for python-app
+# Documentation for ${{values.app_name}}
 
 This application has two endpoints:
 
@@ -9,4 +9,4 @@ Here, you can pretend that I elaborated on what those endpoints do.
 
 ## How to access the app?
 
-You can access the app by accessing this URL: [python-app.azurelaboratory.com/api/v1/healthz](https://python-app.azurelaboratory.com/api/v1/healthz)
+You can access the app by accessing this URL: [${{values.app_name}}.azurelaboratory.com/api/v1/healthz](https://${{values.app_name}}.azurelaboratory.com/api/v1/healthz)
---
+++ python-app/template/src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/details")
+def hello_world():
+    return jsonify(
+        {
+            "time": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+            "hostname": socket.gethostname(),
+            "message": "You are doing well, human!!",
+            "deployed_on": 'kubernetes',
+            'env': '${{ values.app_env }}',
+            'app_name': '${{ values.app_name }}',
+        }
+    )
+
+
+@app.route("/api/v1/healthz")
+def health():
+    return jsonify({"status": "up"}), 200
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0")
